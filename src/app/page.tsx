614cd4abc859f4db0e4169093f705aba0758897b
irfan7k7 <unknown7k7k@gmail.com>
2023-08-14 10:05:35 +0530

    modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,8 @@ const Homepage = () => {
       category: "Stock Market",
       imageUrl: "/Asset/5.jpeg",
       content:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type  ",
+        " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type  ",
+      title: "Technology Changed My Life. Here’s My Story  ",
       author: "irfan",
     };
   });
